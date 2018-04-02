5adef97ab818e1ff937d1a7108fd098bf0f41710
kotaiahandraju <a.kotaiah12@gmail.com>
2018-04-02 18:41:39 +0530

    Customer table changed

--- src/main/java/com/charvikent/abheeSmartHomeSystems/dao/CustomerDao.java
+++ src/main/java/com/charvikent/abheeSmartHomeSystems/dao/CustomerDao.java
@@ -1,13 +1,18 @@
 package com.charvikent.abheeSmartHomeSystems.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.charvikent.abheeSmartHomeSystems.model.Customer;
-import com.charvikent.abheeSmartHomeSystems.model.User;
+import com.charvikent.abheeSmartHomeSystems.model.Customer;
+import com.charvikent.abheeSmartHomeSystems.model.Customer;
+import com.charvikent.abheeSmartHomeSystems.model.Customer;
 
 @Repository
 @Transactional
@@ -29,6 +34,43 @@ public class CustomerDao {
 		return customer;
 	}
 	
+	public Customer checkCustomerExistOrNotByEmail(String email) {
+
+		String hql ="from Customer where  email ='"+email+"'";
+		Query query =entityManager.createQuery(hql);
+
+		List<Customer>usersList =query.getResultList();
+		if(usersList.isEmpty())
+               return null;
+               else
+		return usersList.get(0);
+	}
+public Customer checkCustomerExistOrNotbyMobile(String custMobile) {
+		
+		String hql ="from User where  mobilenumber ='"+custMobile+"'";
+		Query query =entityManager.createQuery(hql);
+
+		@SuppressWarnings("unchecked")
+		List<Customer>usersList =query.getResultList();
+		if(usersList.isEmpty())
+               return null;
+               else
+		return usersList.get(0);
+		
+		
+	}
+
+public Customer checkuserExistOrNot(Customer customer) {
 	
+	String hql =" from User  where (mobilenumber ='"+customer.getMobilenumber()+"') and password='"+customer.getPassword()+"'";
+	Query query =entityManager.createQuery(hql);
+	
+	List<Customer>usersList =query.getResultList();
+	if(usersList.isEmpty())
+           return null;
+           else
+	return usersList.get(0);
+	
+}
 
 }
