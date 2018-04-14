ed2541be928095a34ce43d985cd8743b59cb39ad
Your Name <you@example.com>
2018-04-14 14:28:55 +0530

    changes in task page

--- src/main/java/com/charvikent/abheeSmartHomeSystems/dao/AbheeTaskDao.java
+++ src/main/java/com/charvikent/abheeSmartHomeSystems/dao/AbheeTaskDao.java
@@ -69,8 +69,11 @@ public class AbheeTaskDao {
 
 
 	public List<Map<String, Object>> getInActiveList() {
-		
-		String sql="select * from abhee_task where status='0'";
+		User objuserBean = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+				String sql="select t.id,t.assignto,u.username,t.category as categoryid,s.servicetypename,t.created_time,t.description,t.kstatus,ts.name as statusname,t.priority as priorityid,p.priority,t.severity as severityid,sev.severity, "
+						 + "t.status,t.subject,t.taskdeadline,t.taskno,ab.category,abp.name as modelname "
+						+" FROM abhee_task t,abheeusers u,abheeservicetype s,abheetaskstatus ts,abheepriority p,abheeseverity sev,abheecategory ab ,abhee_product abp"
+						+" where t.assignto=u.id and t.category=ab.id and t.kstatus=ts.id and t.priority=p.id and t.severity=sev.id and t.service_type=s.id and abp.id=t.modelid and t.status='0'and  t.assignto='"+objuserBean.getId()+"'";;
 		List<Map<String,Object>>  inActivelist = jdbcTemplate.queryForList(sql,new Object[]{});
 		return inActivelist;
 	}
