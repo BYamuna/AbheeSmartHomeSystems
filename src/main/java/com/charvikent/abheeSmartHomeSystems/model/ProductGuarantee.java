145f44b37c8ac53c9ed2777fa484625243bcf37c
raju@raju <AITTI@DESKTOP-NRCE29K>
2018-04-20 16:13:46 +0530

    table name adde abhee

--- src/main/java/com/charvikent/abheeSmartHomeSystems/model/ProductGuarantee.java
+++ src/main/java/com/charvikent/abheeSmartHomeSystems/model/ProductGuarantee.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "productguarantee")
+@Table(name = "abheeproductguarantee")
 public class ProductGuarantee 
 {
 	@Id
