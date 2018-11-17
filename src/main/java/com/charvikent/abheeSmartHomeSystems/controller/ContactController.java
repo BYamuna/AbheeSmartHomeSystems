e5ff02121a15da1034cdf6eb9ccffeed8a01f71b
Your Name <you@example.com>
2018-11-17 09:49:31 +0530

    Bugs fixing after testing

--- src/main/java/com/charvikent/abheeSmartHomeSystems/controller/ContactController.java
+++ src/main/java/com/charvikent/abheeSmartHomeSystems/controller/ContactController.java
@@ -1,5 +1,7 @@
 package com.charvikent.abheeSmartHomeSystems.controller;
 
+import javax.mail.MessagingException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.charvikent.abheeSmartHomeSystems.config.SendingMail;
 import com.charvikent.abheeSmartHomeSystems.dao.ContactDao;
 import com.charvikent.abheeSmartHomeSystems.model.Contact;
 
@@ -18,21 +21,23 @@ public class ContactController
 {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ContactController.class);
 	@Autowired ContactDao contactDao;
-	
+	@Autowired SendingMail sendingMail;
 	@RequestMapping(value = "/contact", method = RequestMethod.GET)
 	public String showContactPage(Model model)
 	{
+		LOGGER.debug("Calling contact at controller");
 		model.addAttribute("contact",new Contact());
 		return "contact";	
 	}
 	
 	@RequestMapping(value = "/contact", method = RequestMethod.POST)
-	public String saveContact(@ModelAttribute Contact contact,RedirectAttributes redir)
+	public String saveContact(@ModelAttribute Contact contact,RedirectAttributes redir) throws MessagingException
 	{		
-		LOGGER.debug("Calling contactus at controller");
+		LOGGER.debug("Calling contact at controller");
 		contactDao.saveContact(contact);
 		redir.addFlashAttribute("msg", "Contact details Saved Successfully");
 		redir.addFlashAttribute("cssMsg", "success");
+		sendingMail.sendContactDetailsEmail(contact);
 		return "redirect:contact";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/task.jsp
+++ src/main/webapp/WEB-INF/jsp/task.jsp
@@ -686,7 +686,7 @@
 													var uploadfiles = '';
 													for (var i = 0; i < list.length; i++) {
 														uploadfiles = uploadfiles
-																+ '<a href="reportDocuments/'+list[i]+'" target="_blank" title="'+list[i]+'"><i class="fa fa-paperclip fa-lg grey"></i></a>';
+																+ '<a href="abheeimg/'+list[i]+'" target="_blank" title="'+list[i]+'"><i class="fa fa-paperclip fa-lg grey"></i></a>';
 													}
 													orderObj.uploadfiles = uploadfiles;
 												}
