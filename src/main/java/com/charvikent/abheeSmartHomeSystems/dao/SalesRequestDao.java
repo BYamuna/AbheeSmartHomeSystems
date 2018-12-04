31e0acae1ebd3fcf5cead100ca6ab7726c2ff31a
pleelakrishna <leelakrishnacce@gmail.com>
2018-12-04 17:45:09 +0530

    Api changes on 4/12/2018

--- src/main/java/com/charvikent/abheeSmartHomeSystems/dao/SalesRequestDao.java
+++ src/main/java/com/charvikent/abheeSmartHomeSystems/dao/SalesRequestDao.java
@@ -70,8 +70,9 @@ public class SalesRequestDao
 		public List<Map<String, Object>> getSalesRequestList1(SalesRequest salesRequest)
 		 {
 		 
-			 String hql ="select sr.request_type,sr.salesrequestnumber,ap.name as modelname from abhee_sales_request sr,abhee_product ap where sr.customerid='"+salesRequest.getCustomerid()+"' and sr.modelnumber=ap.id";
-			 System.out.println(hql);
+			/* String hql ="select sr.request_type,sr.salesrequestnumber,ap.name as modelname from abhee_sales_request sr,abhee_product ap where sr.customerid='"+salesRequest.getCustomerid()+"' and sr.modelnumber=ap.id";*/
+			 String hql ="select sr.request_type,sr.salesrequestnumber,sr.modelnumber from abhee_sales_request sr where sr.customerid='"+salesRequest.getCustomerid()+"'"; 
+			System.out.println(hql);
 			
 				List<Map<String,Object>>  retlist = jdbcTemplate.queryForList(hql,new Object[]{});
 				System.out.println(retlist);
@@ -100,8 +101,10 @@ public class SalesRequestDao
 		public List<Map<String, Object>> getSalesRequestListByRequestNo(SalesRequest req)
 		 {
 		 
-			 String hql ="select sr.id,sr.address,sr.email,sr.created_time,sr.imgfiles,sr.location,ap.name as modelname,sr.comments,sr.customerid,sr.mobileno,sr.reqdesc,sr.salesrequestnumber,sr.quotation_documents,sr.enable,sr.customername,sr.request_type\n" + 
-			 		"from abhee_sales_request sr, abhee_product ap where sr.modelnumber=ap.id and salesrequestnumber='"+req.getSalesrequestnumber()+"'";
+			 /*String hql ="select sr.id,sr.address,sr.email,sr.created_time,sr.imgfiles,sr.location,ap.name as modelname,sr.comments,sr.customerid,sr.mobileno,sr.reqdesc,sr.salesrequestnumber,sr.quotation_documents,sr.enable,sr.customername,sr.request_type\n" + 
+			 		"from abhee_sales_request sr, abhee_product ap where sr.modelnumber=ap.id and salesrequestnumber='"+req.getSalesrequestnumber()+"'";*/
+			String hql ="select sr.id,sr.address,sr.email,sr.created_time,sr.imgfiles,sr.location,sr.modelnumber,sr.comments,sr.customerid,sr.mobileno,sr.reqdesc,sr.salesrequestnumber,sr.quotation_documents,sr.enable,sr.customername,sr.request_type" + 
+			 		"from abhee_sales_request sr where salesrequestnumber='"+req.getSalesrequestnumber()+"'";
 			 System.out.println(hql);
 			
 				List<Map<String,Object>>  retlist = jdbcTemplate.queryForList(hql,new Object[]{});
